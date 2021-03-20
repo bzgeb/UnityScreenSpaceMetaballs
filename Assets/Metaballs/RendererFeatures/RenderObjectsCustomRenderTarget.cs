b1da9a2f751506a4a70b2b94f8cb0d716667889c
Bronson Zgeb <bronson.zgeb@gmail.com>
2021-03-20 10:44:18 -0400

    Refactored Blur Pass. Added StepAndClip.

--- Assets/Metaballs/RendererFeatures/RenderObjectsCustomRenderTarget.cs
+++ Assets/Metaballs/RendererFeatures/RenderObjectsCustomRenderTarget.cs
@@ -5,6 +5,8 @@ using UnityEngine.Rendering.Universal;
 
 public class RenderObjectsCustomRenderTarget : ScriptableRendererFeature
 {
+    #region Render Objects
+
     class RenderObjectsPass : ScriptableRenderPass
     {
         readonly int _renderTargetId;
@@ -33,10 +35,12 @@ public class RenderObjectsCustomRenderTarget : ScriptableRendererFeature
         public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
         {
             RenderTextureDescriptor blitTargetDescriptor = renderingData.cameraData.cameraTargetDescriptor;
+            blitTargetDescriptor.colorFormat = RenderTextureFormat.ARGB32;
             cmd.GetTemporaryRT(_renderTargetId, blitTargetDescriptor);
             _renderTargetIdentifier = new RenderTargetIdentifier(_renderTargetId);
-            ConfigureTarget(_renderTargetIdentifier);
-            ConfigureClear(ClearFlag.All, Color.clear);
+
+            ConfigureTarget(_renderTargetIdentifier, renderingData.cameraData.renderer.cameraDepthTarget);
+            ConfigureClear(ClearFlag.Color, Color.clear);
         }
 
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
@@ -58,20 +62,130 @@ public class RenderObjectsCustomRenderTarget : ScriptableRendererFeature
         }
     }
 
+    #endregion
+
+    #region Kawase Blur
+
+    class KawaseBlurRenderPass : ScriptableRenderPass
+    {
+        public Material BlurMaterial;
+        public Material BlitMaterial;
+        public int Passes;
+        public int Downsample;
+
+        int _tmpId1;
+        int _tmpId2;
+
+        RenderTargetIdentifier _tmpRT1;
+        RenderTargetIdentifier _tmpRT2;
+
+        readonly int _blurSourceId;
+        RenderTargetIdentifier _blurSourceIdentifier;
+
+        readonly ProfilingSampler _profilingSampler;
+
+        public KawaseBlurRenderPass(string profilerTag, int blurSourceId)
+        {
+            _profilingSampler = new ProfilingSampler(profilerTag);
+            _blurSourceId = blurSourceId;
+        }
+
+        public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
+        {
+            _blurSourceIdentifier = new RenderTargetIdentifier(_blurSourceId);
+        }
+
+        public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
+        {
+            var width = cameraTextureDescriptor.width / Downsample;
+            var height = cameraTextureDescriptor.height / Downsample;
+
+            _tmpId1 = Shader.PropertyToID("tmpBlurRT1");
+            _tmpId2 = Shader.PropertyToID("tmpBlurRT2");
+            cmd.GetTemporaryRT(_tmpId1, width, height, 0, FilterMode.Bilinear, RenderTextureFormat.ARGB32);
+            cmd.GetTemporaryRT(_tmpId2, width, height, 0, FilterMode.Bilinear, RenderTextureFormat.ARGB32);
+
+            _tmpRT1 = new RenderTargetIdentifier(_tmpId1);
+            _tmpRT2 = new RenderTargetIdentifier(_tmpId2);
+
+            ConfigureTarget(_tmpRT1);
+            ConfigureTarget(_tmpRT2);
+        }
+
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            RenderTextureDescriptor opaqueDesc = renderingData.cameraData.cameraTargetDescriptor;
+            opaqueDesc.depthBufferBits = 0;
+
+            CommandBuffer cmd = CommandBufferPool.Get();
+            using (new ProfilingScope(cmd, _profilingSampler))
+            {
+                // first pass
+                cmd.SetGlobalFloat("_offset", 1.5f);
+                cmd.Blit(_blurSourceIdentifier, _tmpRT1, BlurMaterial);
+
+                for (var i = 1; i < Passes - 1; i++)
+                {
+                    cmd.SetGlobalFloat("_offset", 0.5f + i);
+                    cmd.Blit(_tmpRT1, _tmpRT2, BlurMaterial);
+
+                    // pingpong
+                    var rttmp = _tmpRT1;
+                    _tmpRT1 = _tmpRT2;
+                    _tmpRT2 = rttmp;
+                }
+
+                // final pass
+                cmd.SetGlobalFloat("_offset", 0.5f + Passes - 1f);
+                cmd.Blit(_tmpRT1, renderingData.cameraData.renderer.cameraColorTarget, BlitMaterial);
+            }
+
+            context.ExecuteCommandBuffer(cmd);
+            cmd.Clear();
+
+            CommandBufferPool.Release(cmd);
+        }
+
+        public override void OnCameraCleanup(CommandBuffer cmd)
+        {
+            cmd.ReleaseTemporaryRT(_tmpId1);
+            cmd.ReleaseTemporaryRT(_tmpId2);
+        }
+    }
+
+    #endregion
+
+    #region Renderer Feature
+
     RenderObjectsPass _renderObjectsPass;
+    KawaseBlurRenderPass _blurPass;
 
     const string PassTag = "RenderObjectsCustomRenderTarget";
     [SerializeField] string _renderTargetId;
     [SerializeField] LayerMask _layerMask;
+    [SerializeField] Material _blurMaterial;
+    [SerializeField] Material _blitMaterial;
+    [SerializeField, Range(1, 16)] int _blurPasses = 1;
 
     public override void Create()
     {
         int renderTargetId = Shader.PropertyToID(_renderTargetId);
         _renderObjectsPass = new RenderObjectsPass(PassTag, renderTargetId, _layerMask);
+
+        _blurPass = new KawaseBlurRenderPass("KawaseBlur", renderTargetId)
+        {
+            Downsample = 1,
+            Passes = _blurPasses,
+            BlitMaterial = _blitMaterial,
+            BlurMaterial = _blurMaterial
+        };
     }
 
     public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
     {
         renderer.EnqueuePass(_renderObjectsPass);
+        renderer.EnqueuePass(_blurPass);
     }
+
+    #endregion
 }
---
+++ Assets/Metaballs/Shaders/KawaseBlur.shader
@@ -0,0 +1,68 @@
+﻿Shader "Custom/RenderFeature/KawaseBlur"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                UNITY_FOG_COORDS(1)
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_TexelSize;
+            float4 _MainTex_ST;
+            
+            float _offset;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                return o;
+            }
+
+            fixed4 frag (v2f input) : SV_Target
+            {
+                float2 res = _MainTex_TexelSize.xy;
+                float i = _offset;
+    
+                fixed4 col;                
+                col = tex2D( _MainTex, input.uv );
+                col += tex2D( _MainTex, input.uv + float2( i, i ) * res );
+                col += tex2D( _MainTex, input.uv + float2( i, -i ) * res );
+                col += tex2D( _MainTex, input.uv + float2( -i, i ) * res );
+                col += tex2D( _MainTex, input.uv + float2( -i, -i ) * res );
+                col /= 5.0f;
+                
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
